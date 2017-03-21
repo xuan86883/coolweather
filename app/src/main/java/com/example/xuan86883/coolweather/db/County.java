ced0ef8969aa2b1a11563693b4d78e805718c7c1
xuan86 <xuan86@vip.qq.com>
2017-03-21 14:56:36 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/xuan86883/coolweather/db/County.java
@@ -0,0 +1,44 @@
+package com.example.xuan86883.coolweather.db;
+
+/**
+ * Created by xuan86883 on 2017/3/21.
+ */
+
+public class County {
+    private  int id;
+    private String countyName;
+    private int countyId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCountyId() {
+        return countyId;
+    }
+
+    public void setCountyId(int countyId) {
+        this.countyId = countyId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/example/xuan86883/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package com.example.xuan86883.coolweather.db;
+
+/**
+ * Created by xuan86883 on 2017/3/21.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
